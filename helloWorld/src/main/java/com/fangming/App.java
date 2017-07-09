e0ab33ca436329ecb60d5d653aeb68c9641d76f8
Jack <Jack@lylian-MacB.local>
2017-07-09 16:42:24 +0800

    initial commit

--- helloWorld/src/main/java/com/fangming/App.java
+++ helloWorld/src/main/java/com/fangming/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Edit by Jack 275287908!" );
         System.out.println( "Hello Git!" );
         System.out.println( "Hello Git, as well!" );
     }
